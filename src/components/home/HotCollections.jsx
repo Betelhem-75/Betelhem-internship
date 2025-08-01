e01f2066193c9fb116322e60593517488a715ba7
betelhem-75 <betelhemfikadu75gmail.com>
2025-08-01 10:19:16 -0500

    message

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -2,17 +2,21 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Slider from "react-slick";
 import axios from "axios";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 
 const HotCollections = () => {
   const [collections, setCollections] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function feachCollections() {
       const { data } = await axios.get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections`);
       setCollections(data);
+      setLoading(false);
       console.log(data);
 
     }
@@ -22,7 +26,27 @@ const HotCollections = () => {
   const settings = {
     arrows: true,
     slidesToShow: 4,
-    infinite: true
+    infinite: true,
+    responsive: [
+    {
+      breakpoint: 1192, // screen width <= 996px
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 768, // screen width <= 768px
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 480, // screen width <= 480px
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
     
   };
 
@@ -37,12 +61,21 @@ const HotCollections = () => {
             </div>
           </div>
           <Slider {...settings}> 
-            {collections.map(collection => (
-              <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12 card" key={collection.authorId}>
-                <div className="nft_coll">
+            { loading
+               ? new Array(10).fill(0).map((_, index) => (
+                   <div style={{ padding: "10px", textAlign: "center" }} key={index}>
+                     <Skeleton height={60} style={{ marginBottom: 10 }} />
+                     <Skeleton circle height={60} width={60} style={{ margin: "auto", marginBottom: 10 }} />
+                     <i className="fa fa-check"></i>
+                     <Skeleton width={100} height={20} style={{ margin: "auto", marginBottom: 5 }} />
+                     <Skeleton width={60} height={15} style={{ margin: "auto" }} />
+                   </div>
+                ))
+            : collections.map(collection => (
+                <div className="nft_coll" key={collection.authorId}>
                   <div className="nft_wrap" >
                     <Link to="/item-details">
-                      <img src={collection.nftImage} className="lazy img-fluid card-top-image" alt=""  />
+                      <img src={collection.nftImage} className="lazy img-fluid" alt=""  />
                     </Link>
                  </div>
                  <div className="nft_coll_pp">
@@ -58,8 +91,8 @@ const HotCollections = () => {
                    <span>ERC-{collection.code}</span>
                  </div>
                </div>
-             </div>
-           ))}
+             ))
+           }
            </Slider>
         </div>
       </div>
